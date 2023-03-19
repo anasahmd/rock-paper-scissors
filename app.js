03fb979d56e59c795d2d39f650ec1633953d1f5a
anasahmd <anasahmad0239@gmail.com>
2023-03-19 16:03:47 +0530

    Add game function

--- app.js
+++ app.js
@@ -7,30 +7,56 @@ const getComputerChoice = () => {
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    return `It's a Tie! ${playerSelection} ties with ${computerSelection}`;
+    return {
+      message: `It's a Tie! ${playerSelection} ties with ${computerSelection}`,
+      winner: 'none',
+    };
   }
   if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-      return 'You Lose! Paper beats Rock';
+      return { message: 'You Lose! Paper beats Rock', winner: 'computer' };
     } else {
-      return 'You Win! Rock beats Scissors';
+      return { message: 'You Win! Rock beats Scissors', winner: 'player' };
     }
   } else if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
-      return 'You Win! Paper beats Rock';
+      return { message: 'You Win! Paper beats Rock', winner: 'player' };
     } else {
-      return 'You Lose! Scissors beats Paper';
+      return { message: 'You Lose! Scissors beats Paper', winner: 'computer' };
     }
   } else if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
-      return 'You Lose! Rock beats Scissors';
+      return { message: 'You Lose! Rock beats Scissors', winner: 'computer' };
     } else {
-      return 'You Win! Scissors beats Paper';
+      return { message: 'You Win! Scissors beats Paper', winner: 'player' };
     }
+  } else {
+    return { message: 'Invalid input', winner: 'none' };
   }
 };
 
-const playerSelection = 'Rock';
-const computerSelection = getComputerChoice();
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
 
-console.log(playRound(playerSelection, computerSelection));
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt();
+    const { message, winner } = playRound(playerSelection, getComputerChoice());
+    if (winner == 'player') {
+      playerScore++;
+    } else if (winner == 'computer') {
+      computerScore++;
+    }
+    console.log(message);
+  }
+
+  if (playerScore > computerScore) {
+    console.log('Player Wins!');
+  } else if (computerScore < playerScore) {
+    console.log('Computer Wins!');
+  } else {
+    console.log('Tie Game!');
+  }
+};
+
+game();
